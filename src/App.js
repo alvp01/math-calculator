c38a8d704114d7ea88b51cc9c5309d1dcec89957
Abel Lavieri <abel.lavieri@gmail.com>
2022-01-05 18:20:39 -0400

    Change calculator and app components to be stateless functional

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="container">
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,74 +1,64 @@
-import React from 'react';
+import { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.displayValues = this.displayValues.bind(this);
-  }
+const Calculator = () => {
+  const [calcObj, setCalcObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(btnName) {
-    const newState = calculate(this.state, btnName);
-    this.setState(() => ({
-      total: newState.total,
-      next: newState.next,
-      operation: newState.operation,
-    }));
-  }
+  const handleClick = (btnName) => {
+    const newState = calculate(calcObj, btnName);
+    setCalcObj(newState);
+  };
 
-  displayValues() {
-    const { total, next, operation } = this.state;
+  const displayValues = ({ total, next, operation }) => {
     if (total && !next && !operation) return `${total}`;
     if (!total && next && !operation) return `${next}`;
     if (total && !next && operation) return `${total} ${operation}`;
     if (total && next && operation) return `${next}`;
     return '0';
-  }
+  };
 
-  render() {
-    return (
-      <div className="calculator-body">
-        <div className="screen">{this.displayValues()}</div>
-        <div className="keyboard">
-          <div className="row">
-            <button type="button" className="btn" onClick={() => this.handleClick('AC')}>AC</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('+/-')}>+/-</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('%')}>%</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('÷')}>÷</button>
-          </div>
-          <div className="row">
-            <button type="button" className="btn" onClick={() => this.handleClick('7')}>7</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('8')}>8</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('9')}>9</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('x')}>x</button>
-          </div>
-          <div className="row">
-            <button type="button" className="btn" onClick={() => this.handleClick('4')}>4</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('5')}>5</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('6')}>6</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('-')}>-</button>
-          </div>
-          <div className="row">
-            <button type="button" className="btn" onClick={() => this.handleClick('1')}>1</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('2')}>2</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('3')}>3</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('+')}>+</button>
-          </div>
-          <div className="row last-row">
-            <button type="button" className="btn" onClick={() => this.handleClick('0')}>0</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('.')}>.</button>
-            <button type="button" className="btn" onClick={() => this.handleClick('=')}>=</button>
-          </div>
+  return (
+    <div className="calculator-body">
+      <div className="screen">
+        { displayValues(calcObj) }
+      </div>
+      <div className="keyboard">
+        <div className="row">
+          <button type="button" className="btn" onClick={() => handleClick('AC')}>AC</button>
+          <button type="button" className="btn" onClick={() => handleClick('+/-')}>+/-</button>
+          <button type="button" className="btn" onClick={() => handleClick('%')}>%</button>
+          <button type="button" className="btn" onClick={() => handleClick('÷')}>÷</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={() => handleClick('7')}>7</button>
+          <button type="button" className="btn" onClick={() => handleClick('8')}>8</button>
+          <button type="button" className="btn" onClick={() => handleClick('9')}>9</button>
+          <button type="button" className="btn" onClick={() => handleClick('x')}>x</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={() => handleClick('4')}>4</button>
+          <button type="button" className="btn" onClick={() => handleClick('5')}>5</button>
+          <button type="button" className="btn" onClick={() => handleClick('6')}>6</button>
+          <button type="button" className="btn" onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button type="button" className="btn" onClick={() => handleClick('1')}>1</button>
+          <button type="button" className="btn" onClick={() => handleClick('2')}>2</button>
+          <button type="button" className="btn" onClick={() => handleClick('3')}>3</button>
+          <button type="button" className="btn" onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div className="row last-row">
+          <button type="button" className="btn" onClick={() => handleClick('0')}>0</button>
+          <button type="button" className="btn" onClick={() => handleClick('.')}>.</button>
+          <button type="button" className="btn" onClick={() => handleClick('=')}>=</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
