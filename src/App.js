1682134094d14f4d5a89e489e7c9319601e745c2
Abel Lavieri <abel.lavieri@gmail.com>
2022-01-10 20:21:32 -0400

    Add calculator page and Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import React from 'react';
-import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import CalculatorPage from './pages/CalculatorPage';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <div className="container">
-    <Calculator />
-  </div>
+  <Router>
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
---
+++ src/pages/CalculatorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div>
+    <h2>Let&apos;s do some calculations!</h2>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
